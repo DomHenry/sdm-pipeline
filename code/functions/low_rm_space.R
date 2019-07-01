67e5e605ace29c8b90b1925c83b378c2224bf3b0
Dominic Henry <dominichenry@gmail.com>
2019-07-01 10:52:23 +0200

    Removed old files

--- code/functions/low_rm_space.R
+++
@@ -1,6 +0,0 @@
-# Col names to lower case and replace whitespace --------------------------
-low_rm_space <- function(x){
-  x %>% 
-    select_all(tolower) %>% 
-    select_all(funs(str_replace_all(.," ","_")))
-}
