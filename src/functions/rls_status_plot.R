ae82b54eae911b9edfe5c82e2505094a9d6a7c3f
Dominic Henry <dominichenry@gmail.com>
2019-07-04 16:04:32 +0200

    Minor change to gghighlight function.

--- src/functions/rls_status_plot.R
+++ src/functions/rls_status_plot.R
@@ -9,7 +9,7 @@ rls_status_plot <- function(x,rlscat,spp_rm){
   
   ggplot(x %>% filter(!scientificname %in% spp_rm))+
     geom_sf(color = "red",size = 1.5)+
-    gghighlight(rls == rlscat)+
+    gghighlight(rls == rlscat)+ 
     geom_sf(data = sa_merge, fill = alpha("grey",0))+
     labs(title = glue::glue("{rlscat} species occurrence records"),
          subtitle = glue::glue("Total of {nspp} species"))
