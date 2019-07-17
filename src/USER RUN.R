49a190f4b7ec5464d072df4052e25baf868f23f5
Dominic Henry <dominichenry@gmail.com>
2019-07-17 16:33:00 +0200

    Reworked the flow of sourcing code files and added code to render Rmd report file.

--- src/USER RUN.R
+++ src/USER RUN.R
@@ -1,11 +1,23 @@
-source("code/03_SDM import data.R")
+
+# Source steps 3 - 7 ------------------------------------------------------
+source("src/03_SDM import data.R")
 rm(list = ls())
-source("code/04_blockCV.R")
+source("src/04_blockCV.R")
 rm(list = ls())
-source("code/05_SDM MaxEnt.R")
+source("src/05_SDM MaxEnt.R")
 rm(list = ls())
-source("code/06_SDM ensembles.R")
+source("src/06_SDM ensembles.R")
 rm(list = ls())
-source("code/07_prepare results for extraction.R")
+source("src/07_prepare results for extraction.R")
 rm(list = ls())
 
+# Render Rmd file ---------------------------------------------------------
+query <- readxl::read_xlsx("data input/SDM_query.xlsx") %>% print
+sppselect <- query$Value[which(query$Input == "Species")]
+rmarkdown::render(input = glue::glue("data output/rmarkdown documents/{sppselect}/SDM report.Rmd"),
+                  output_format = "html_document",
+                  output_dir = glue::glue("data output/rmarkdown documents/{sppselect}"))
+rm(list = ls())
+
+# Source step 8 -----------------------------------------------------------
+source("src/08_Rename report and send to wd().R")
